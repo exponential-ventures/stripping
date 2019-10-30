4bc0192e95424c526817fa9270e62e64eeb2ac28
Thales Ribeiro <thalessr@gmail.com>
2019-10-30 12:01:09 -0500

    Add basic unit tests

---
+++ tests/test_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##
+## ----------------
+## |              |
+## | CONFIDENTIAL |
+## |              |
+## ----------------
+##
+## Copyright Exponential Ventures LLC (C), 2019 All Rights Reserved
+##
+## Author: Thales Ribeiro <thales@xnv.io>
+##
+## If you do not have a written authorization to read this code
+## PERMANENTLY REMOVE IT FROM YOUR SYSTEM IMMEDIATELY.
+##
+
+import asynctest
+from os.path import split, join
+import shutil
+
+from stripping.cache import StepCache
+from stripping.executor import Context
+from stripping import setup_stripping
+
+tmp_dir = join(split(__file__)[0], '.test_cache')
+
+st, context = setup_stripping(tmp_dir)
+
+@st.step
+def test_step():
+   return 'Hello'
+
+
+class TestCache(asynctest.TestCase):
+    def setUp(self):
+        self.step_cache = StepCache()
+
+    def tearDown(self):
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+
+    async def test_execute_or_retrieve(self):
+        self.step_cache.register_context(context)
+        step = st.steps[0]
+        reponse = await self.step_cache.execute_or_retrieve(step)
+        self.assertEqual('Hello', reponse)
