65039d9c905fe8aab4fa379a5d9ede811cc120fc
Thales Ribeiro <thalessr@gmail.com>
2019-11-06 11:58:03 -0600

    Add cache invalidation

--- stripping/cache.py
+++ stripping/cache.py
@@ -10,17 +10,24 @@
 ## Copyright Exponential Ventures LLC (C), 2019 All Rights Reserved
 ##
 ## Author: Adriano Marques <adriano@xnv.io>
+## Author: Thales Ribeiro  <thales@xnv.io>
 ##
 ## If you do not have a written authorization to read this code
 ## PERMANENTLY REMOVE IT FROM YOUR SYSTEM IMMEDIATELY.
 ##
 
-
+import asyncio
 import inspect
+from glob import glob
+import os
+import datetime
 
 from .exceptions import StepNotCached
 from .singleton import SingletonDecorator
 from .storage import CacheStorage
+
+ACCESS = 'acess'
+DIR_PATH = 'path'
 
 
 @SingletonDecorator
@@ -30,6 +37,11 @@ class StepCache:
     def __init__(self, cache_dir):
         self.cache_dir = cache_dir
         self.storage = CacheStorage(self.cache_dir)
+
+        self.cache_invalidation = CacheInvalidation()
+        self.cache_invalidation.add_dir(self.cache_dir)
+        asyncio.ensure_future(self.cache_invalidation.strategy_runner())
+
 
     def register_context(self, context):
         self.context = context
@@ -49,3 +61,56 @@ class StepCache:
             self.storage.save_step(step_fn.code, step_return, self.context, *args, **kwargs)
 
             return step_return
+
+
+@SingletonDecorator
+class CacheInvalidation:
+
+
+    def __init__(self):
+        self.__cached_dirs = {}
+
+    def add_dir(self, cache_dir):
+        self.__cached_dirs[cache_dir] = {}
+
+    def force_delete(self, cache_dir):
+        import shutil
+
+        if cache_dir not in self.__cached_dirs:
+            pass
+
+        shutil.rmtree(cache_dir, ignore_errors=True)
+
+    def strategy(self):
+        """
+            Removes cached files which haven't being accessed for 1 year or more
+        """
+        
+        one_year_ago_timestamp = datetime.datetime.timestamp(self.year_ago(1))
+        for d in self.__cached_dirs.keys():
+            for dir_path in glob('{}/*'.format(d)):
+                self.__cached_dirs[d] = {}
+                self.__cached_dirs[d][DIR_PATH] = dir_path
+                self.__cached_dirs[d][ACCESS] = self.__last_access(dir_path)
+                if self.__cached_dirs[d][ACCESS] <= one_year_ago_timestamp:
+                    self.force_delete(self.__cached_dirs[d][DIR_PATH])
+
+
+    async def strategy_runner(self):
+        while True:
+            self.strategy()
+            await asyncio.sleep(60)
+
+
+    def __last_access(self, path):
+        """
+            Returns when the dir was last accessed
+        """
+        return os.path.getatime(path)
+
+
+    def year_from_now(self, years : int = 1):
+        return datetime.datetime.now() + datetime.timedelta(days=years*365)
+
+    def year_ago(self, years: int = 1):
+        return datetime.datetime.now() - datetime.timedelta(days=years*365)
