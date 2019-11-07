ca22838197cc3080232f3a3ca4265de76e26d6bc
Nathan S. Martins <nathan.eua@gmail.com>
2019-11-07 10:50:23 -0300

    Simpler test.

--- tests/test_cache_with_catalysis.py
+++ tests/test_cache_with_catalysis.py
@@ -1,5 +1,3 @@
-import json
-import os
 import unittest
 
 from stripping import setup_stripping_with_catalysis
@@ -7,18 +5,6 @@ from stripping import setup_stripping_with_catalysis
 
 class TestCacheWithCatalysis(unittest.TestCase):
 
-    def setUp(self) -> None:
-        super().setUp()
-        self.credentials = json.dumps({
-            "local": {
-                "driver": "local",
-                "path": "/tmp/catalysis/",
-            }
-        })
-
     def test_has_catalysis_client(self):
-        tmp_dir = os.path.join(os.path.split(__file__)[0], '.test_cache')
-
-        st, _ = setup_stripping_with_catalysis(tmp_dir, self.credentials)
-
+        st, _ = setup_stripping_with_catalysis("/tmp/", "local")
         self.assertIsNotNone(st.cache.storage.catalysis_client)
