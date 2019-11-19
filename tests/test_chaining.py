01ad02bb6b65b6f839e272cf67ea57e0f5ed1efb
Nathan S. Martins <nathan.eua@gmail.com>
2019-11-19 11:38:28 -0300

    Minor test refactoring and adding regular steps only test.
    Signed-off-by: Nathan S. Martins <nathan.eua@gmail.com>

--- tests/test_chaining.py
+++ tests/test_chaining.py
@@ -6,10 +6,8 @@ from stripping import setup_stripping
 
 class ChainingTestCase(TestCase):
 
-    def test_chain(self):
-        tmp_dir = f"/tmp/{str(uuid.uuid4())}/cache/"
-
-        st, context = setup_stripping(tmp_dir)
+    def test_chain_only(self):
+        st, context = self._setUpStripping()
 
         @st.chain
         def test_chain_step_1():
@@ -23,11 +21,7 @@ class ChainingTestCase(TestCase):
         self.assertEqual('Hello World!', r)
 
     def test_mixed_chain(self):
-        tmp_dir = f"/tmp/{str(uuid.uuid4())}/cache/"
-
-        st, context = setup_stripping(tmp_dir)
-        st.steps = list()
-        st.chain_steps = list()
+        st, context = self._setUpStripping()
 
         @st.step
         def test_step_1():
@@ -51,3 +45,33 @@ class ChainingTestCase(TestCase):
 
         # Since the last step is not a chain and it doesn't return anything, the execute result is empty.
         self.assertEqual(None, r)
+
+    def test_regular_steps_only(self):
+        st, context = self._setUpStripping()
+
+        @st.step
+        def test_step_1():
+            print("Running regular step #1.")
+
+        @st.step
+        def test_step_2():
+            print("Running regular step #2.")
+
+        @st.step
+        def test_step_3():
+            print("Running regular step #3.")
+
+        r = st.execute()
+        self.assertEqual(None, r)
+
+    @staticmethod
+    def _setUpStripping():
+        tmp_dir = f"/tmp/{str(uuid.uuid4())}/cache/"
+
+        st, context = setup_stripping(tmp_dir)
+
+        # Resetting lists to prevent cross-test contamination
+        st.steps = list()
+        st.chain_steps = list()
+
+        return st, context
